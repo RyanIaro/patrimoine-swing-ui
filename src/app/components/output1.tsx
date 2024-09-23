62ea9210fd9ce7cd00298e44864e9463c2cb89ea
RyanIaro <hei.ryan.3@gmail.com>
2024-09-23 11:17:58 +0300

    style: output1 structure

---
+++ src/app/components/output1.tsx
@@ -0,0 +1,12 @@
+export default function Output1() {
+  return (
+    <div className="flex flex-col size-full rounded-sm">
+      <div className="h-2/5 border-t-2 border-r border-l-2 border-gray-500 bg-white m-1 shadow-md">
+        !! FLUX IMPOSSIBLES !! <br/>
+      </div>
+      <div className="h-3/5 border-t-2 border-r border-l-2 border-gray-500 bg-white m-1 shadow-md">
+      !! FLUX JOURNALIERS !! <br/>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 import Input from "@/app/components/input"
+import Output1 from "@/app/components/output1";
 
 export default function Home() {
   return (
     <>
       <div className="w-2/5 h-full flex flex-col border-black border rounded-md text-sm">
         <Input/>
-        <div className="flex flex-col size-full border-black border rounded-sm">
-          <div className="w-full h-2/5">
-          output-1-1
-          </div>
-          <div className="w-full h-3/5">
-          output-1-2
-          </div>
-        </div>
+        <Output1/>
       </div>
       <div className="w-3/5 h-full border-black border rounded-sm">output-2</div>
     </>
