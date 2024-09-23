f8b146abcfb2c8472e26b7682c91b9dc70f4fd00
RyanIaro <hei.ryan.3@gmail.com>
2024-09-23 10:58:41 +0300

    style: input structure

---
+++ src/app/components/input.tsx
@@ -0,0 +1,41 @@
+export default function Input() {
+  return (
+    <div className="flex flex-col h-28 gap-1">
+      <div className="m-1">
+        <label htmlFor="possesseur">Patrimoine</label>
+        <select className="ml-1 rounded-sm shadow-md" name="possesseur" id="possesseur">
+          <option value="Crésus">Crésus</option>
+          <option value="Myriade fr">Myriade fr</option>
+        </select>
+      </div>
+      <div className="flex justify-around">
+        <div>
+          <input type="checkbox" name="Agrégat" id="Agrégat" />
+          <label className="ml-1" htmlFor="Agrégat">Agrégat</label>
+        </div>
+        <div>
+          <input type="checkbox" name="Trésorerie" id="Trésorerie" />
+          <label className="ml-1" htmlFor="Trésorerie">Trésorerie</label>
+        </div>
+        <div>
+          <input type="checkbox" name="Immobilisation" id="Immobilisation" />
+          <label className="ml-1" htmlFor="Immobilisation">Immobilisation</label>
+        </div>
+        <div>
+          <input type="checkbox" name="Obligations" id="Obligations" />
+          <label className="ml-1" htmlFor="Obligations">Obligations</label>
+        </div>
+      </div>
+      <div className="m-1 flex">
+        <div>
+          <label htmlFor="De">De: </label>
+          <input className="mr-1 rounded-sm shadow-md" type="date" name="De" id="De" defaultValue={new Date().toISOString().slice(0,10)}/>
+        </div>
+        <div>
+          <label htmlFor="À">À: </label>
+          <input className="mr-1 rounded-sm shadow-md" type="date" name="À" id="À" defaultValue="2024-12-01"/>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import Input from "@/app/components/input"
+
 export default function Home() {
   return (
     <>
-      <div className="w-2/5 h-full flex flex-col border-black border rounded-sm">
-        <div className="w-full h-28 border-black border rounded-sm">
-          input
-        </div>
+      <div className="w-2/5 h-full flex flex-col border-black border rounded-md text-sm">
+        <Input/>
         <div className="flex flex-col size-full border-black border rounded-sm">
           <div className="w-full h-2/5">
           output-1-1
